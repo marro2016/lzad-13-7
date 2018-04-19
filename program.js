cc7f22dc5640356d258454da0f3f2826eaaa28a1
Marcin Różański <rozanskimarcin2016@gmail.com>
2018-04-19 22:27:01 +0200

    Add

--- program.js
+++ program.js
@@ -1,29 +1,39 @@
-var OSinfo = require('./modules/OSinfo');
 
-process.stdin.setEncoding('utf-8');
+var fs = require('fs');
+var StatMode = require('stat-mode');
+var colors = require('colors');
+
+fs.stat('./cat.jpeg', function(err, stats) {
+  var statMode = new StatMode(stats);
+  console.log(statMode.toString());
+});
+
+var fs = require('fs');
+
+fs.readFile('./text.txt', 'utf-8', function(err, data) {
+  console.log('Dane przed zapisem'.blue);
+  console.log(data);
+  fs.appendFile('./text.txt', '\nA tak wyglądaja dane po zapisie', function(err) {
+    if (err) throw err;
+    console.log('Zapisano'.blue);
+    fs.readFile('./text.txt', 'utf-8', function(err, data) {
+      console.log('Dane po zapisie'.blue);
+      console.log(data);
+    });
+  });
+});
 
 
-process.stdin.on('readable', function() {
-  var input = process.stdin.read();
-  if (input !== null) {
-    process.stdout.write(input);
-    var instruction = input.toString().trim();
-    switch (instruction) {
-      case 'version':
-        console.log('Node version: ' + process.versions.node);
-        break;
-      case 'language':
-        console.log('Language is: ' + process.env.LANG);
-        break;
-      case '/exit':
-        process.stdout.write('Quitting app!\n');
-        process.exit();
-        break;
-      case '/getOSinfo':
-        OSinfo.print();
-        break;
-      default:
-        process.stderr.write('Wrong instruction!\n');
-    }
+fs.readdir('./', 'utf-8', function(err, files) {
+  if (err) {throw err;}
+  else {
+    fs.writeFile('./nowy.txt', files, function() {
+      if (err) throw err;
+      console.log('Zapisano zawartość katalogu do pliku nowy.txt'.red);
+    });
+
+    fs.readFile('./nowy.txt', 'utf-8', function(err, data) {
+      console.log('Zawartość katalogu to: '.green + data);
+    });
   }
 });
